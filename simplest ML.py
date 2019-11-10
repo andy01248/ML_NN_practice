7cf17c3d4fc80bbf2f45309aa86e7d50d8164d30
S.Ren <55898103+andy01248@users.noreply.github.com>
2019-11-10 02:47:56 +0000

    first ML code finish

--- simplest ML.py
+++ simplest ML.py
@@ -2,17 +2,35 @@ import numpy as np
 
 def sigmoid(x):
     return 1/(1+np.exp(-x))
+def sigmoid_derivative(x):
+    return x*(1-x)
     
-training_inputs=[[1,0,1,0],
+training_inputs=np.array([[1,0,1,0],
         [1,0,0,1],
         [1,1,0,0],
-        [1,1,1,1]]
-training_outputs=np.array([(1,1,0,0)]).T
-print(training_inputs)
+        [1,1,1,1],
+        [0,0,0,0],
+        [0,0,1,1],
+        [0,1,0,1],
+        [0,1,1,0]])
+training_outputs=np.array([(1,1,0,0,1,1,0,0)]).T
 np.random.seed(1)
 weights=2*(np.random.random((4,1)))-1
-# print(weights)
-outputs=np.dot(training_inputs,weights)
-print(outputs)
-cost=outputs-training_outputs
-print(cost)
+print('initial weights are',weights)
+# print(training_inputs)
+# print(training_inputs.T)
+for i in range(2000):
+    outputs=sigmoid(np.dot(training_inputs,weights))
+    #print('current outputs are',outputs)
+    error=training_outputs-outputs
+    #print('current errors are',error)
+    adjustment=error*sigmoid_derivative(outputs)
+    weights+=np.dot(training_inputs.T,adjustment) #Every same elements
+    #weight adjustment
+
+print('adjusted weights are',weights)
+print('current outputs are',outputs)
+print('current errors are',error)
+new_inputs=[0,1,1,1]
+new_outputs=sigmoid(np.dot(new_inputs,weights))
+print('new outputs are', new_outputs)
